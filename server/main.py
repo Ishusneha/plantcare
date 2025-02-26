5f43219c5e4d92170184a9717c9a0b79ee49d9f8
bishal2059 <bishal.sap21@gmail.com>
2025-02-27 01:37:38 +0545

    bug fixes

--- server/main.py
+++ server/main.py
@@ -63,9 +63,5 @@ def predict():
 
     # return send_file(img_io, mimetype='image/jpeg')
 
-@app.route('/<path:subpath>')
-def catch_all(subpath):
-    return "Hello, World!"
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
